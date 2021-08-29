e903e2be3d9affa570fa85e1a204949d769b95ba
cronos550m <cronos550m@gmail.com>
2021-08-29 10:37:15 -0300

    se acomoda register y se crear popup con terminos y condiciones

--- src/public/js/register.js
+++ src/public/js/register.js
@@ -7,3 +7,18 @@ checkbox.addEventListener("change", function() {
     checked ? (btn.disabled = false) : (btn.disabled = true);
   }
 });
+
+var btnOpenPopupTerm = document.getElementById('btn-open-popup-term'),
+    overlayTerm = document.getElementById('overlay-term'),
+    popupTerm = document.getElementById('popup-term'),
+    btnClosePopupTerm = document.getElementById('btn-close-popup-term');
+
+btnOpenPopupTerm.addEventListener('click', function(){
+    overlayTerm.classList.add('active');
+    popupTerm.classList.add('active');
+});
+
+btnClosePopupTerm.addEventListener('click', function(){
+    overlayTerm.classList.remove('active');
+    popupTerm.classList.remove('active');
+});
