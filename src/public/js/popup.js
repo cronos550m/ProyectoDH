1d6bfe8b1154d3ff04867317222d16f8b5687f05
cronos550m <cronos550m@gmail.com>
2021-08-29 00:33:21 -0300

    se le da color al sidebar

--- src/public/js/popup.js
+++ src/public/js/popup.js
@@ -2,11 +2,6 @@ var btnOpenPopup = document.getElementById('btn-open-popup-login'),
     overlay = document.getElementById('overlay-login'),
     popup = document.getElementById('popup-login'),
     btnClosePopup = document.getElementById('btn-close-popup-login');
-    // btnOpenPopup = document.getElementById('btn-open-popup-register'),
-    // overlay = document.getElementById('overlay-register'),
-    // popup = document.getElementById('popup-register'),
-    // btnClosePopup = document.getElementById('btn-close-popup-register');
-
 
 btnOpenPopup.addEventListener('click', function(){
     overlay.classList.add('active');
