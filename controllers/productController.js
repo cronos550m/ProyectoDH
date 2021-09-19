a5bfbea471545a6602f9f10747b04c59a490bfea
lpagutti <lpagnutti@gmail.com>
2021-09-19 18:13:06 -0300

    punto 1,2 listos 3(1-2-3)listos

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,18 +1,28 @@
 /* este controlador se encarga de relacionar los eventos de los productos con las vistas*/
-const path = require('path'); // modulo para relacionar las vistas
+const path = require('path'); // modulo para relacionar las rutas
+const dbProductos = require("../db/product.json");// levanto la base de datos de productos
+const fs = require("fs"); // requiero el modulo fs para poder leer archivos y escribirlos
 
-const controller={
-    productDetail:(req,res)=>{ 
-        res.render(path.join(__dirname,'../src/views/products/productDetail.ejs')) //devuelve la vista del detalle de producto en este evento
+
+const controller = {
+    productDetail: (req, res) => {
+        const id = req.params.id; // uso el id que viene desde la ruta
+        const producto = dbProductos.find(item => item.id == id); // busco el id en la base
+        res.render(path.join(__dirname, '../src/views/products/productDetail.ejs'), { product: producto }) //devuelve la vista del detalle de producto y paso como parametro un prodructo
     },
-    productCart:(req,res)=>{ 
-        res.render(path.join(__dirname,'../src/views/products/productCart.ejs')) //devuelve la vista del carro de compras en este evento
+    productCart: (req, res) => {
+        res.render(path.join(__dirname, '../src/views/products/productCart.ejs')) //devuelve la vista del carro de compras en este evento
     },
-    productNew:(req,res)=>{ 
-        res.render(path.join(__dirname,'../src/views/products/productNew.ejs')) //devuelve la vista adicion y edicion de producto en este evento
+    productNew: (req, res) => {
+        res.render(path.join(__dirname, '../src/views/products/productNew.ejs')) //devuelve la vista adicion y edicion de producto en este evento
     },
-    products:(req,res)=>{ 
-        res.render(path.join(__dirname,'../src/views/products/products.ejs')) //devuelve la vista de los productos en este evento
+    products: (req, res) => {
+        res.render(path.join(__dirname, '../src/views/products/products.ejs'), { products: dbProductos })
+        //devuelve la vista de los productos en este evento, paso como parametro la variable con todo el array de productos
+    },
+    insertProduct: (req, res) => { // verificar porque no me llegan cosas del body
+        const newId = dbProductos[(dbProductos.length) - 1].id + 1  // busco el ultimo id y le sumo 1
+        console.log(req.body);
     }
 };
 
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -5,10 +5,11 @@ const router = express.Router(); // asigno el modulo router a la constante route
 const productController = require ('../controllers/productController'); /*requiero el controlador product (este contiene todas las rutas referidas a los productos)
                                                                     ..para salir de la carpeta*/
 
-router.get('/productDetail', productController.productDetail);// al acceder al detalle de producto llamo al controlador en el metodo productDetail
+router.get('/productDetail/:id', productController.productDetail);// al acceder al detalle de producto llamo al controlador en el metodo productDetail con su id
 router.get('/productCart', productController.productCart);// al acceder al carro de compras llamo al controlador en el metodo productCart
 router.get('/productNew', productController.productNew);// al acceder al productNew  llamo al controlador en el metodo productNew
 router.get('/products', productController.products);// al acceder al products  llamo al controlador en el metodo products
+router.post('/productNew',productController.insertProduct);// cuando llega el metodo post de product new accedo a esta ruta
 
 
 module.exports = router;
