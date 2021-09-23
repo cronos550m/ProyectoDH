815f5240d4bef184676fd92d95a25a4068db7e25
lpagutti <lpagnutti@gmail.com>
2021-09-22 21:21:36 -0300

    index routes y controller modificados corregir css

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,9 +1,10 @@
 /* este controlador se encarga de relacionar los eventos del main con las vistas*/
 const path = require('path'); // modulo para relacionar las vistas
+const dbProductos = require("../db/product.json");// levanto la base de datos de productos
 
 const controller={
     home: (req,res)=>{ 
-        res.render(path.join(__dirname,'../src/views/index.ejs')) //devuelve la vista del index en este evento
+        res.render(path.join(__dirname,'../src/views/index.ejs'), { products: dbProductos }) //devuelve la vista del index en este evento
     }
 };
 
