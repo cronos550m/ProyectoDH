3fcf6668201f37bc8bd32fb8dd03c74f86dad252
lpagutti <lpagnutti@gmail.com>
2021-10-27 16:01:10 -0300

    finished auth mid

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,7 +4,9 @@ const express = require('express'); // requiero express para trabajar con el mod
 const router = express.Router(); // asigno el modulo router a la constante router
 const multerMiddleware = require('../middleware/multerMiddleware') // Mid para trabajar con envios de archivos
 const validationMiddleware = require('../middleware/validationMiddleware') //mid para validaciones no es utilizado
-const guestMiddleware = require('../middleware/guestMiddleware') // mid para validar sesiones
+const guestMiddleware = require('../middleware/guestMiddleware') // mid para validar sesion
+const authtMiddleware = require('../middleware/authMiddleware') // mid para validar que no haya nadie en sesion
+
 const userController = require ('../controllers/userController.js'); /*requiero el controlador main (este contiene todas las rutas referidas al main)
                                                                     ..para salir de la carpeta*/
 
@@ -13,7 +15,7 @@ const userController = require ('../controllers/userController.js'); /*requiero 
 router.get('/register', guestMiddleware, userController.userRegister);// accedo al formulario de registro controlando la sesion
 router.get('/login', guestMiddleware, userController.userLogin);// accedo al formulario de login controlando la sesion
 router.get('/forget', userController.userForget);// al acceder al forget  llamo al controlador en el metodo forget
-router.get('/profile', userController.userProfile);// al acceder al forget  llamo al controlador en el metodo profile
+router.get('/profile', authtMiddleware, userController.userProfile);// al acceder al forget  llamo al controlador en el metodo profile
 router.post('/register',multerMiddleware.single('imagen'), userController.userInsert); // procesar el registro de usuario
 router.post('/login',userController.userLoginProcess); // proceso el login de usuario
 module.exports = router;
